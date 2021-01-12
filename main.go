34de4caca9da46551be7b2adc723ae17ee853422
lambdaret <lambdaret@gmail.com>
2021-01-12 15:40:18 +0900

    dictionary type function

--- main.go
+++ main.go
@@ -9,23 +9,16 @@ import (
 
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
 
-	err := dictionary.Add(word, definition)
-
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
-	} 
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "definition:", hello)
-
-	err2 := dictionary.Add(word, definition)
-
-	if err2 != nil {
-		fmt.Println(err2)
-	} 
-	
+	}
+	fmt.Println(word)
 
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,8 +6,11 @@ import "errors"
 type Dictionary map[string] string
 
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That word already exists")
+var (
+	errNotFound = errors.New("Not Found")
+	errCantUpdate = errors.New("Cant update non-exising word")
+	errWordExists = errors.New("That word already exists")
+)
 
 // Search ...
 func (d Dictionary) Search(word string) (string, error) {
@@ -33,4 +36,21 @@ func (d Dictionary) Add (word string, def string) error {
 	// 	return errWordExists
 	// }
 	return nil
+}
+
+// Update ...
+func (d Dictionary) Update (word, definition string) error{
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete ...
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
