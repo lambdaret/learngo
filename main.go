2eacd00c8802c0c54073cbab92f4dbf7d8c23f94
lambdaret <lambdaret@gmail.com>
2021-01-12 14:08:36 +0900

    struct

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 
+type person struct {
+	name string
+	age int
+	favFood []string
+}
+
 func main() {
-	nico := map[string] string {"name": "nico", "age": "12"}
-	for key, value :=range nico {
-		fmt.Println(key, value)
-	}
-	fmt.Println(nico)
+	favFood := [] string{"kimchi", "ramen"}
+	// nico := person {"nico", 18, favFood}
+	nico := person {name:"nico", age:18, favFood:favFood}
+
+	fmt.Println(nico.name)
 }
