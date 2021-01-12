6c531154ea53b6f06b99f2d2cd3f482a87f03373
lambdaret <lambdaret@gmail.com>
2021-01-12 16:05:34 +0900

    urlcheck

--- main.go
+++ main.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/lambdaret/learngo/mydict"
+	"net/http"
 )
 
-
+var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	var results = make(map[string]string)
+	urls := []string {
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://www.academy.nomadcoders.co/",
+		"https://www.raddit.com/",
 	}
-	fmt.Println(word)
 
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	// fmt.Println(results)
+	for url, result := range(results) {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println("resp:", resp)
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil	
 }
